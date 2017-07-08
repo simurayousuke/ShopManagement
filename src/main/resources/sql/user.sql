150b1e8ace3b473f185e36bfe7b2b49155f31b56
Jerry <842791049@qq.com>
2017-07-08 13:57:28 +0800

    修改使用邮箱查询用户的sql语句

--- src/main/resources/sql/user.sql
+++ src/main/resources/sql/user.sql
@@ -5,5 +5,5 @@ SELECT * FROM t_user WHERE username = #para(0)
 SELECT * FROM t_user WHERE phone = #para(0)
 #end
 #sql("findByEmail")
-SELECT t_user.id AS id, username, salt, pwd, uuid, avator, phone FROM t_user RIGHT JOIN t_email ON t_email.user_id = t_user.id WHERE email = #para(0);
+SELECT * FROM t_user WHERE email = #para(0);
 #end
