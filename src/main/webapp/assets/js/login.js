a63a608078de600658582e21f360c6b6a0ab9713
志庄 杨 <yzz@mail.dlut.edu.cn>
2017-07-10 21:14:51 +0800

    修改验证码发送机制

--- src/main/webapp/assets/js/login.js
+++ src/main/webapp/assets/js/login.js
@@ -8,16 +8,33 @@ $(document).ready(function () {
 
     var captchaInput = $('#captcha');
 
+
     $('#send-button').click(function () {
         var captcha = captchaInput.val();
+        var that = this;
+        that.prop('disabled', true);
         $.post('/captcha/phone', {captcha: captcha}, function (data) {
             if (data.status) {
                 $.msg('发送成功');
+                var count = 59;
+                var timer = setInterval(function () {
+                    that.prop('value', count-- + 's');
+                    if(count===0){
+                        that.prop('value','重新获取');
+                        that.removeProp('disabled');
+                        clearInterval(timer);
+                    }
+                }, 1000);
             } else {
                 $.msg('发送失败');
                 updateCaptcha();
+                that.removeProp('disabled');
             }
+        },function(){
+            $.msg('网络异常');
+            that.removeProp('disabled');
         });
+
     });
 
     updateCaptcha();
@@ -49,7 +66,7 @@ $(document).ready(function () {
             if (data.status) {
                 location.href = '/';
             } else {
-                $.msg('用户名或密码错误');
+                $.msg(data.msg);
                 updateCaptcha();
             }
         });
@@ -78,7 +95,8 @@ $(document).ready(function () {
             if (data.status) {
                 location.href = '/';
             } else {
-                $.msg('邮箱或密码错误');
+                $.msg(data.msg);
+                updateCaptcha();
             }
         });
     });
@@ -101,7 +119,8 @@ $(document).ready(function () {
             if (data.status) {
                 location.href = '/';
             } else {
-                $.msg('验证码错误');
+                $.msg(data.msg);
+                updateCaptcha();
             }
         });
     });
