81fab3279445ce0b9532f15200d589595485d22d
志庄 杨 <yzz@mail.dlut.edu.cn>
2017-07-11 20:31:42 +0800

    修改push.sh

--- push.sh
+++ push.sh
@@ -1,11 +1,34 @@
-git pull
-echo ----------------------------------
+case $1 in
+    a|A|all|ALL|y|Y|yes|Yes)
+        git pull
+		echo ----------------------------------
+        ;;
+esac
 git add *
 echo ----------------------------------
-git commit -m "$1"
+echo -n "Continue?[y]/n"
+read ANS
+case $ANS in
+    n|N|no|No)
+        exit 0
+        ;;
+esac
+git commit -m "$2"
 echo ----------------------------------
 git push
 echo ----------------------------------
+case $1 in
+    a|A|all|ALL)
+        ;;
+	*)
+		read ANS2
+		case $ANS2 in
+			n|N|no|No)
+				exit 0
+				;;
+		esac
+		;;
+esac
 git push huawei
 echo ----------------------------------
 git push github
