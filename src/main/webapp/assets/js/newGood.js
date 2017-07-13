0dd9fd48ad078548fed190bcd7f663ee27209441
Forrest <727469926@qq.com>
2017-07-13 11:29:15 +0800

    1.商品上架页面完成 2.修改为驼峰式命名

--- src/main/webapp/assets/js/newGood.js
+++ src/main/webapp/assets/js/newGood.js
@@ -15,3 +15,13 @@ KindEditor.create('textarea.kindeditor', {
     ]
 });
 
+$(document).ready(function () {
+    var captchaImg = $('.captcha');
+
+    var updateCaptcha = function () {
+        captchaImg.prop('src', '/captcha/image?v=' + Math.random());
+    };
+
+    updateCaptcha();
+});
+
