cb9e9ec25013f469a68237019a2b361509712e68
Forrest <727469926@qq.com>
2017-07-14 17:09:02 +0800

    搜索提示下拉表单键盘监听完成

--- src/main/webapp/assets/js/suggest.js
+++ src/main/webapp/assets/js/suggest.js
@@ -13,27 +13,58 @@ $(document).ready(function () {
             $('#search-button').click();
         }
 
-
-        $.post('/suggest/' + $('#search-word').val(), {}, function (data) {
-            if (data.status) {
-                var json = JSON.parse(data.suggestions);
-                if (json.length <= 0) {
-                    hideSuggest();
-                } else {
-                    suggestion.empty();
-                    for (var i = 0; i < json.length; ++i) {
-                        suggestion.append('<li>' + json[i].suggestion + '</li>');
-                        console.log(json[i].suggestion);
+        if (suggestion.css('display') == 'block' && x == 38 || x == 40) {
+            var current = suggestion.find('li.hover');
+            if (x == 38) {
+                if (current.length > 0) {
+                    var prevLi = current.removeClass('hover').prev();
+                    if (prevLi.length > 0) {
+                        prevLi.addClass('hover');
+                        input.val(prevLi.html());
                     }
-                    $('#gov_search_suggest').css('display', 'block');
+                } else {
+                    var last = suggestion.find('li:last');
+                    last.addClass('hover');
+                    input.val(last.html());
                 }
-            } else {
-                hideSuggest();
-                console.log('发送失败');
+
+            } else if (x == 40) {
+                if (current.length > 0) {
+                    var nextLi = current.removeClass('hover').next();
+                    if (nextLi.length > 0) {
+                        nextLi.addClass('hover');
+                        input.val(nextLi.html());
+                    }
+                } else {
+                    var first = suggestion.find('li:first');
+                    first.addClass('hover');
+                    input.val(first.html());
+                }
             }
-        }, function () {
-            console.log('网络异常');
-        })
+        } else {
+            $.post('/suggest/' + $('#search-word').val(), {}, function (data) {
+                if (data.status) {
+                    var json = JSON.parse(data.suggestions);
+                    if (json.length <= 0) {
+                        hideSuggest();
+                    } else {
+                        suggestion.empty();
+                        for (var i = 0; i < json.length; ++i) {
+                            suggestion.append('<li>' + json[i].suggestion + '</li>');
+                            console.log(json[i].suggestion);
+                        }
+                        $('#gov_search_suggest').css('display', 'block');
+                    }
+                } else {
+                    hideSuggest();
+                    console.log('发送失败');
+                }
+            }, function () {
+                console.log('网络异常');
+            })
+        }
+
+
 
     });
     function hideSuggest() {
