910551eb1adeb86b04670528090c617f28ee44bb
志庄 杨 <yzz@mail.dlut.edu.cn>
2017-07-14 12:10:13 +0800

    增加搜索提示

--- src/main/java/cn/enbug/shop/suggest/SuggestController.java
+++ src/main/java/cn/enbug/shop/suggest/SuggestController.java
@@ -2,8 +2,11 @@ package cn.enbug.shop.suggest;
 
 import cn.enbug.shop.common.controller.BaseController;
 import cn.enbug.shop.common.kit.Ret;
+import cn.enbug.shop.common.kit.UrlKit;
 import cn.enbug.shop.common.service.OpenSearchService;
 import com.jfinal.aop.Before;
+import com.jfinal.aop.Clear;
+import com.jfinal.ext.interceptor.NoUrlPara;
 import com.jfinal.ext.interceptor.POST;
 
 import java.io.IOException;
@@ -15,14 +18,20 @@ import java.io.IOException;
  */
 public class SuggestController extends BaseController {
 
+    @Clear(NoUrlPara.class)
     @Before(POST.class)
     public void index() throws IOException {
-        String key=getPara();
-        String result=OpenSearchService.ME.suggest("default",key);
-        if(null == result){
+        String key = getPara();
+        if (null == key) {
             renderJson(Ret.fail("Fail"));
-        }else {
-            renderJson(Ret.succeed().set("suggestions",result));
+            return;
+        }
+        key = UrlKit.decode(key, "utf-8");
+        String result = OpenSearchService.ME.suggest("default", key);
+        if (null == result) {
+            renderJson(Ret.fail("Fail"));
+        } else {
+            renderJson(Ret.succeed().set("suggestions", result));
         }
     }
 
