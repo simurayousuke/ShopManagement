b7835ada8ecab21c1a4234dd24cec5b1c8f04f12
志庄 杨 <yzz@mail.dlut.edu.cn>
2017-07-17 23:37:43 +0800

    增加浏览器控制台输出

--- src/main/webapp/assets/js/console.js
+++ src/main/webapp/assets/js/console.js
@@ -1,5 +1,8 @@
-console.log('%c 几处早莺争暖树，', 'color:#bada55');
-console.log('%c EnBug团队也需要新鲜的血液，', 'color:#bada55');
-console.log('%c 若君有意，', 'color:#bada55');
-console.log('%c 请发送邮件至yzz@ssdut153.cn，', 'color:#bada55');
-console.log('%c              ———— EnBug Group.', 'color:#bada55');
+console.log('%c 几处早莺争暖树，', 'color:#bada55; font-size: 16px;');
+console.log('%c EnBug团队也需要新鲜的血液，', 'color:#bada55; font-size: 16px;');
+console.log('%c 若君有意，', 'color:#bada55; font-size: 16px;');
+console.log('%c 请发送邮件至yzz@ssdut153.cn，', 'color:#bada55; font-size: 16px;');
+console.log('%c              ———— EnBug Group.', 'color:#bada55; font-size: 16px;');
+console.log('');
+console.log("%c%s", "color: red; background: yellow; font-size: 24px; font-weight: bold;", "安全警告!");
+console.log("%c%s", "color: black; font-size: 18px;", "请勿在此控制台输入或粘贴你不明白的代码，以避免攻击者窃取你的信息来冒充你。");
