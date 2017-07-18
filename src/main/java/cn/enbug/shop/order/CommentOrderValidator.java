541bbea1da1d52af7be8af5a97c5c2fabd0ccf76
Jerry <842791049@qq.com>
2017-07-18 22:05:10 +0800

    增加订单部分的拦截器

---
+++ src/main/java/cn/enbug/shop/order/CommentOrderValidator.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2017 EnBug Group.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.enbug.shop.order;
+
+import cn.enbug.shop.common.kit.Ret;
+import cn.enbug.shop.common.validator.BaseValidator;
+import com.jfinal.core.Controller;
+
+/**
+ * @author Hu Wenqiang
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class CommentOrderValidator extends BaseValidator {
+
+    @Override
+    protected void validate(Controller c) {
+        validateRequired("order", Ret.MSG, "请输入订单号");
+        validateInteger("id", Ret.MSG, "请输入商品id");
+    }
+
+    @Override
+    protected void handleError(Controller c) {
+        c.setAttr(Ret.STATUS, false);
+        c.renderJson();
+    }
+
+}
