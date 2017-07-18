02cc3da5ff71d86aaa0bf3f4880955e07a133323
Forrest <727469926@qq.com>
2017-07-18 10:54:34 +0800

    用户中心头像上传

--- src/main/webapp/assets/js/userInfo.js
+++ src/main/webapp/assets/js/userInfo.js
@@ -6,6 +6,13 @@ $(document).ready(function () {
             var data = JSON.parse(responseObject.response);
             if (!data.status) {
                 return data.msg;
+            } else {
+                $('#create-user-avator').val(data.url);
+                $('.user-photo').prop('src', '//smcdn.yangzhizhuang.net/' + data.url);
+                var files = $('.file');
+                for (var i = 0; i < files.length; ++i) {
+                    files[i].style.display = 'none';
+                }
             }
         },
         filters: {
@@ -17,5 +24,21 @@ $(document).ready(function () {
             ],
             max_file_size: '5mb'
         }
+    });
+
+    $('#set-user-photo').click(function () {
+        $.post('/user/avator', {avator: $('#create-user-avator').val()}, function (data) {
+            if (data.status) {
+                $.warn('上传成功', function () {
+                    location.reload();
+                });
+            } else {
+                $.alert('警告', '上传失败');
+
+            }
+        }, function () {
+            $.alert('警告', '网络异常');
+
+        });
     });
 });
