5286a3b8510a52dafae9b3fc8314df359d862f6d
志庄 杨 <yzz@mail.dlut.edu.cn>
2017-07-18 12:19:38 +0800

    增加店铺链接
    Signed-off-by: 志庄 杨 <yzz@mail.dlut.edu.cn>

--- src/main/java/cn/enbug/shop/store/StoreController.java
+++ src/main/java/cn/enbug/shop/store/StoreController.java
@@ -11,14 +11,18 @@ import com.jfinal.ext.interceptor.GET;
 /**
  * @author Forrest Yang
  * @author Yang Zhizhuang
- * @version 1.0.1
+ * @version 1.0.2
  * @since 1.0.0
  */
 @Before({GET.class, UserInterceptor.class})
 public class StoreController extends BaseController {
 
     public void index() {
-        Shop shop = ShopService.ME.findShopByUuid(getPara());
+        String para = getPara();
+        Shop shop = ShopService.ME.findShopByUuid(para);
+        if (null == shop) {
+            shop = ShopService.ME.findShopById(Integer.parseInt(para));
+        }
         setAttr("shop", shop);
         setAttr("list", GoodService.ME.getGoodListByShopId(shop.getId()));
         render("index.html");
