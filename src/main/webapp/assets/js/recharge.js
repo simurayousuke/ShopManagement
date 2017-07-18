268a452139bd6b73c44b9ed9fdbb2829c7cc6052
志庄 杨 <yzz@mail.dlut.edu.cn>
2017-07-18 13:01:56 +0800

    优化搜索
    Signed-off-by: 志庄 杨 <yzz@mail.dlut.edu.cn>

--- src/main/webapp/assets/js/recharge.js
+++ src/main/webapp/assets/js/recharge.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     $('#charge-button').click(function () {
-        var value = $('#charge-value').val();
+        var input = $('#charge-value');
+        var value = input.val();
         if (!$.validatePriceFormat(value)) {
             $.alert('警告', '格式不正确');
             return;
@@ -8,6 +9,7 @@ $(document).ready(function () {
         $.post('/user/modify/charge', {value: value}, function (data) {
             if (data.status) {
                 $.warn('充值成功！', function () {
+                    input.val('');
                     location.reload();
                 });
             } else {
