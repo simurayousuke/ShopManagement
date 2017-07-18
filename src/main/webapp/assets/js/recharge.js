116d49d2e8ca18238a547fca2b62e9ff73355598
志庄 杨 <yzz@mail.dlut.edu.cn>
2017-07-18 12:50:03 +0800

    增加充值功能
    Signed-off-by: 志庄 杨 <yzz@mail.dlut.edu.cn>

--- src/main/webapp/assets/js/recharge.js
+++ src/main/webapp/assets/js/recharge.js
@@ -1,13 +1,17 @@
 $(document).ready(function () {
     $('#charge-button').click(function () {
-        $.post('/user/modify/charge', {value: $('#charge-value').val()}, function (data) {
+        var value = $('#charge-value').val();
+        if (!$.validatePriceFormat(value)) {
+            $.alert('警告', '格式不正确');
+            return;
+        }
+        $.post('/user/modify/charge', {value: value}, function (data) {
             if (data.status) {
                 $.warn('充值成功！', function () {
                     location.reload();
                 });
             } else {
                 $.alert('警告', data.msg);
-
             }
         }, function () {
             $.alert('警告', '网络异常');
