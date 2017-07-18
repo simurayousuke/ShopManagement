0d3818bf3d6e1824afd9a5a0f2d201429b5f84a3
志庄 杨 <yzz@mail.dlut.edu.cn>
2017-07-18 10:56:05 +0800

    增加修改头像

---
+++ doc/user.md
@@ -0,0 +1,30 @@
+# 用户信息相关
+
+<ol>
+<li>设置头像
+
+请求地址：/user/avator
+
+参数:
+
+* avator 头像key
+
+</li>
+</ol>
+
+* 返回值:
+
+成功:
+```json
+{
+  "status": true
+}
+```
+
+失败:
+```json
+{
+  "status": false,
+  "msg": ""
+}
+```
---
+++ src/main/java/cn/enbug/shop/user/center/UserCenterService.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2017 EnBug Group.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.enbug.shop.user.center;
+
+import cn.enbug.shop.common.kit.RedisKit;
+import cn.enbug.shop.common.kit.Ret;
+import cn.enbug.shop.common.model.User;
+
+/**
+ * @author Yang Zhizhuang
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class UserCenterService {
+
+    public static final UserCenterService ME = new UserCenterService();
+
+    public Ret setAvator(String token, String avator) {
+        User user = RedisKit.getUserByToken(token);
+        if (null == user) {
+            return Ret.fail("登录超时");
+        }
+        user.setAvator(avator);
+        return user.update() ? Ret.succeed() : Ret.fail("设置失败");
+    }
+
+}
