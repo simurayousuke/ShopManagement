30117652b3fedaf726359a022979a3d4fa9a8cd9
Jerry <842791049@qq.com>
2017-07-19 14:55:14 +0800

    修复修改密码的问题，增加修改密码js

---
+++ src/main/webapp/assets/js/modifyPwd.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+
+    var repeat = $('#repeat-pwd');
+
+    $('#pwd-form').submit(function (e) {
+        e.preventDefault();
+        var $this = $(this);
+        var data = $this.serializeObject();
+        var oldPwd = data.old_pwd;
+        var newPwd = data.new_pwd;
+        var repeatPwd = repeat.val();
+        if (oldPwd.trim() === '') {
+            $.msg('请输入旧密码');
+            return;
+        }
+        if (newPwd.trim() === '') {
+            $.msg('请输入新密码');
+            return;
+        }
+        if (newPwd !== repeatPwd) {
+            $.msg('两次密码不一致');
+            return;
+        }
+        $.post('/user/center/modifypwd', data, function (data) {
+            if (!data.status) {
+                $.msg(data.msg);
+            } else {
+                $.msg('修改成功');
+                $this[0].reset();
+            }
+        });
+    });
+
+});
