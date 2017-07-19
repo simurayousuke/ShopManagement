d79ad3297b387035264e28ca602648670cbd5235
Forrest <727469926@qq.com>
2017-07-19 10:37:12 +0800

    评论增加头像

--- src/main/resources/sql/comment.sql
+++ src/main/resources/sql/comment.sql
@@ -23,5 +23,5 @@ SELECT * FROM t_comment WHERE good_id = #para(0) AND is_good = 0 ORDER BY id;
 SELECT * FROM t_comment WHERE shop_id = #para(0) AND is_good = 0 ORDER BY id;
 #end
 #sql("findRecordByGoodId")
-SELECT username, is_good, description, comment_time FROM t_comment INNER JOIN t_user ON buyer_id = t_user.id WHERE good_id = #para(0);
+SELECT avator, username, is_good, description, comment_time FROM t_comment INNER JOIN t_user ON buyer_id = t_user.id WHERE good_id = #para(0);
 #end
